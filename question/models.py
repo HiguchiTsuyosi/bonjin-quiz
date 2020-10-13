defa95007d621edd3a978fac482303227437acd7
Higuchi Tsuyoshi <t-higuchi@matsusaka.co.jp>
2020-10-13 13:10:37 +0900

    commit-2

--- question/models.py
+++ question/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Question(models.Model):
+    question=models.TextField()
+    answer=models.BooleanField()
+    comment=models.TextField()
